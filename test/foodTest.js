05e07019630fae5fe64ebb6da252091cbb61c5c8
Your Name <nskong@ucdavis.edu>
2021-05-28 23:09:53 -0700

    add test outline skeleton

---
+++ test/foodTest.js
@@ -0,0 +1,34 @@
+const food = require("../server/food/food");
+const expect = require("chai").expect;
+
+describe("Food Calculations", function() {
+    describe("Red Meat Emissions", function() { 
+        it ("should be true", function () { 
+            expect(true).to.equal(true)
+        })
+    })
+
+    describe("White Meat Emissions", function() { 
+    })
+
+    describe("Dairy Emissions", function() { 
+    })
+
+    describe("Cereals Emissions", function() { 
+    })
+
+    describe("Vegetables Emissions", function() { 
+    })
+
+    describe("Fruit Emissions", function() { 
+    })
+
+    describe("Oils Emissions", function() { 
+    })
+
+    describe("Snacks Emissions", function() { 
+    })
+
+    describe("Drinks Emissions", function() { 
+    })
+})
---
+++ test/travelTest.js
@@ -0,0 +1,22 @@
+const travel = require("../server/travel/travel");
+const expect = require("chai").expect;
+
+describe("Travel Calculations", function() {
+    describe("Vehicle emissions", function() { 
+        it ("should be true", function () { 
+            expect(true).to.equal(true)
+        })
+    })
+
+    describe("Bus emissions", function() { 
+    })
+
+    describe("Metro emissions", function() { 
+    })
+
+    describe("Rail emissions", function() { 
+    })
+
+    describe("Flying emissions", function() { 
+    })
+})
